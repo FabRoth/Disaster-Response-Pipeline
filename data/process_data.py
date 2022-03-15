4bdf780a500a9692b92eaf56de0ea9005f4d0360
Fabian Roth <roth@re-invent.de>
2022-03-15 15:57:44 +0100

    Filled Templates with content from the jupyter notebooks in course

--- data/process_data.py
+++ data/process_data.py
@@ -71,7 +71,7 @@ def save_data(df, database_filename):
     '''
 
     engine = create_engine(f'sqlite:///{database_filename}')
-    df.to_sql(database_filename, engine, index=False)
+    df.to_sql('MessagesAndCategories', engine, index=False)
 
 
 
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,105 @@
 import sys
 
+import re
+import pandas as pd
+from sqlalchemy import create_engine
+import joblib
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+nltk.download(['punkt', 'wordnet'])
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.ensemble import RandomForestClassifier
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.metrics import classification_report, accuracy_score
+
 
 def load_data(database_filepath):
-    pass
+    '''
+    Laodding data
+    
+    Input:
+    
+    Output:
+    
+    '''
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('MessagesAndCategories', engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = Y.columns
 
+    return X,Y, category_names
 
 def tokenize(text):
-    pass
+    '''Tokenize
+    
+    Input:
+    
+    Output:
+    
+    '''
+    # Replace all urls with a urlplaceholder string
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    
+    # Extract all the urls from the provided text 
+    detected_urls = re.findall(url_regex, text)
+    
+    # Replace url with a url placeholder string
+    for detected_url in detected_urls:
+        text = text.replace(detected_url, 'url_place_holder_string')
 
+    # Extract the word tokens from the provided text
+    tokens = nltk.word_tokenize(text)
+    
+    #Lemmanitizer to remove inflectional and derivationally related forms of a word
+    lemmatizer = nltk.WordNetLemmatizer()
+
+    # List of clean tokens
+    clean_tokens = [lemmatizer.lemmatize(w).lower().strip() for w in tokens]
+
+    return clean_tokens
 
 def build_model():
-    pass
+    '''TODO'''
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    
+    parameters = {
+        'clf__estimator__min_samples_split': [2, 3, 4]  
+    }
+
+    cv = GridSearchCV(pipeline, param_grid = parameters)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''TODO'''
+
+    Y_prediction_test = model.predict(X_test)
+
+    for category_name in category_names:
+        print('Category: {} '.format(category_name))
+        print(classification_report(Y_test[category_name].values, Y_prediction_test[:,list(category_names).index(category_name)]))
+        print('Accuracy {}\n\n'.format(accuracy_score(Y_test[category_name].values, Y_prediction_test[:,list(category_names).index(category_name)])))
 
 
 def save_model(model, model_filepath):
-    pass
+    '''TODO'''
+    joblib.dump(model, model_filepath)
 
 
 def main():
